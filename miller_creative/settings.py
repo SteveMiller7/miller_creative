784c6f04b7f1d540231e077f8b850d22710bbce2
SteveMiller7 <info@millercreativedesign.com>
2022-08-11 11:45:33 +0000

    added static to settings.py - all connected and working

--- miller_creative/settings.py
+++ miller_creative/settings.py
@@ -141,6 +141,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
