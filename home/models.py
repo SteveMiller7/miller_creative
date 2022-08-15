b918b7e4c697112cb73d04329b0e7f33dc57a9dc
SteveMiller7 <info@millercreativedesign.com>
2022-08-15 12:15:28 +0000

    Added Featured Company model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Featured_Company (models.Model):
+    company = models.CharField(max_length=50)
+    company_type = models.CharField(max_length=50)
+    featured_image = CloudinaryField('image', default='placeholder')
+    date = models.CharField(max_length=20)
+    description = models.CharField(max_length=300)
+    location = models.CharField(max_length=50)
+
+
