ecec7591586df6a255661882edf16ca32faa4379
SteveMiller7 <info@millercreativedesign.com>
2022-08-16 15:00:33 +0000

    Added products views and templates

--- products/admin.py
+++ products/admin.py
@@ -7,6 +7,7 @@ class ProductAdmin(admin.ModelAdmin):
         'sku',
         'name',
         'description',
+        'size',
         'quantity',
         'laminate',
         'price',
--- products/models.py
+++ products/models.py
@@ -45,8 +45,9 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    quantity = models.IntegerField(choices=QUANTITY_LIST)
-    laminate = models.IntegerField(choices=LAMINATE_LIST)
+    size = models.IntegerField(choices=SIZE_LIST, null=True, blank=True)
+    quantity = models.IntegerField(choices=QUANTITY_LIST, null=True, blank=True)
+    laminate = models.IntegerField(choices=LAMINATE_LIST, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
