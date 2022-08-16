d3026262993382116914f1552b65e6350e86fc20
SteveMiller7 <info@millercreativedesign.com>
2022-08-16 12:29:54 +0000

    Added category and product models

--- home/models.py
+++ home/models.py
@@ -5,10 +5,56 @@ from cloudinary.models import CloudinaryField
 class FeaturedPicture(models.Model):
     picture = CloudinaryField('image', default='placeholder')
 
-    
+
 class FeaturedCompany(models.Model):
     picture = models.ForeignKey('FeaturedPicture', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, null=True, blank=True)
     location = models.CharField(max_length=50, null=True, blank=True)
     date = models.CharField(max_length=20, null=True, blank=True)
     description = models.CharField(max_length=300, null=True, blank=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    QUANTITY_LIST = (
+        (0, '100'),
+        (1, '250'),
+        (2, '500'),
+        (3, '1000'),
+        (4, '2500'),
+        (5, '5000'),
+    )
+
+    SIZE_LIST = (
+        (0, 'A6'),
+        (1, 'A5'),
+        (2, 'A5'),
+    )
+
+    LAMINATE_LIST = (
+        (0, 'Matt'),
+        (1, 'Gloss'),
+        (2, 'Soft Touch'),
+    )
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.IntegerField(choices=QUANTITY_LIST)
+    size = models.IntegerField(choices=SIZE_LIST)
+    laminate = models.IntegerField(choices=LAMINATE_LIST)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
