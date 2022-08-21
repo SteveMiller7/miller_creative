01969d4201d2f9bc8d97e5ef5d1f5bb0d554a323
SteveMiller7 <info@millercreativedesign.com>
2022-08-21 23:03:50 +0000

    Styled various elements throughtout site

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LZHYMDFAQv2Hr9sMYnBe7sRTp3T2Tf6a454xUi2T8mKaFvMawiF1aVV8JOHn2FpuF2FjGEcQvJTIvY2tW4HxQUC00cbEcJCqn',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
--- static/js/index.js
+++
@@ -1,8 +0,0 @@
-var window.onscroll = function () {
-    scrollRotate();
-};
-
-function scrollRotate() {
-    let image = document.getElementById("spin-logo");
-    image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
-}
